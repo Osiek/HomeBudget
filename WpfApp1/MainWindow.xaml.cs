6cd6b22faed411c03bd68907108dbbb4b22a9762
Patryk Rubaj <194799@edu.p.lodz.pl>
2018-06-20 09:14:06 +0200

    soulution dir structure changed

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User MyUser;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyUser = new User();
+            MyUser.FirstName = "Mark";
+
+            Binding myBinding = new Binding("MyUserProperties");
+            myBinding.Mode = BindingMode.TwoWay;
+            myBinding.Source = MyUser.FirstName;
+
+            BindingOperations.SetBinding(UserFirstNameTextBlock, TextBlock.TextProperty, myBinding);
+
+            //UserFirstNameTextBlock.SetBinding(TextBlock.TextProperty, myBinding);
+        }
+
+        private void ChangeFirstName_Click(object sender, RoutedEventArgs e)
+        {
+            MyUser.FirstName = "Patryk";
+        }
+    }
+}
---
+++ WpfApp1/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string name;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ID { get; set; }
+        public string FirstName {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanges("FirstName");
+            }
+        }
+        public string LastName { get; set; }
+
+        protected void OnPropertyChanges(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        
+    }
+}
