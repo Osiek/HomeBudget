1dc435055aaebfe439e10bafd84d15a8f2a9410f
Patryk Rubaj <194799@edu.p.lodz.pl>
2018-06-27 12:13:37 +0200

    added LINQ query to get values for spendings summary by shop for selected period of time

--- HomeBudget/Controllers/ReportController.cs
+++ HomeBudget/Controllers/ReportController.cs
@@ -9,20 +9,57 @@ namespace HomeBudget.Controllers
 {
     class ReportController : BaseController
     {
-        public Dictionary<Shop, double> GetShopSummaryValues(string begin, string end)
+        public Dictionary<Shop, decimal> GetShopSummaryValues(string begin, string end)
         {
+            Dictionary<Shop, decimal> shopsSummary = new Dictionary<Shop, decimal>();
             DateTime dateBegin;
             DateTime dateEnd;
 
             DateTime.TryParse(begin + " 00:00:00", out dateBegin);
             DateTime.TryParse(end + " 23:59:59", out dateEnd);
 
-            var query = from e in db.Entries
-                        where e.Date > dateBegin && e.Date < dateEnd
-                        select e;
+            var query = from entry in db.Entries
+                        where entry.Date > dateBegin && entry.Date < dateEnd
+                        group entry by entry.ShopID into shopsGroup
+                        select new {
+                            Shop = shopsGroup.FirstOrDefault().Shop,
+                            Price = shopsGroup.Sum(_ => _.Price)
+                        };
 
+            foreach (var shop in query)
+            {
+                System.Console.WriteLine("{0} = {1}", shop.Shop, shop.Price);
+                shopsSummary.Add(shop.Shop, shop.Price);
+            }
 
-            return null;
+            return shopsSummary;
+        }
+
+        public Dictionary<Shop, decimal> GetCategoriesSummaryValues(string begin, string end)
+        {
+            Dictionary<Shop, decimal> shopsSummary = new Dictionary<Shop, decimal>();
+            DateTime dateBegin;
+            DateTime dateEnd;
+
+            DateTime.TryParse(begin + " 00:00:00", out dateBegin);
+            DateTime.TryParse(end + " 23:59:59", out dateEnd);
+
+            var query = from category in db.Categories
+                        where category.Date > dateBegin && category.Date < dateEnd
+                        group entry by entry.ShopID into shopsGroup
+                        select new
+                        {
+                            Shop = shopsGroup.FirstOrDefault().Shop,
+                            Price = shopsGroup.Sum(_ => _.Price)
+                        };
+
+            foreach (var shop in query)
+            {
+                System.Console.WriteLine("{0} = {1}", shop.Shop, shop.Price);
+                shopsSummary.Add(shop.Shop, shop.Price);
+            }
+
+            return shopsSummary;
         }
     }
 }
